468f810067e765a4e4189f11dbaa44cdd8c5b2ea
Martin Facorro <mfacorro@live.com.ar>
2023-05-01 12:06:44 -0300

    14. Construcción de la Capa de Infraestructura de Persistencia de Datos

---
+++ Packagroup.Ecommerce/Packagroup.Ecommerce.Infraestructura.Data/ConnectionFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Packagroup.Ecommerce.Transversal.Common;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Packagroup.Ecommerce.Infraestructura.Data
+{
+    public class ConnectionFactory : IConnectionFactory
+    {
+        //RESPONSABILIDAD: CONECTARSE A LA BASE DE DATOS Y DEVOLVER LA INSTANCIA DE LA CONEXION.
+
+        private readonly IConfiguration _configuration; // NOS PERMITE ACCEDER A LAS PROPIEDADES DE LOS DIFERENTES PROYECTOS(APPSETINGS.JSON)
+
+        public ConnectionFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IDbConnection GetConnection // CON ESTE METODO ACCEDEMOS A LA BASE DE DATOS Y NOS DEVUELVE UNA INSTANCIA DE CONEXION ABIERTA
+        {
+            get
+            {
+                var sqlConnection = new SqlConnection();
+
+                if (sqlConnection == null) 
+                    return null;
+
+                sqlConnection.ConnectionString = _configuration.GetConnectionString("NorthwingConnection");
+                sqlConnection.Open();
+
+                return sqlConnection;
+
+            }
+        }
+
+    }
+}
---
+++ Packagroup.Ecommerce/Packagroup.Ecommerce.Transversal.Common/IConnectionFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace Packagroup.Ecommerce.Transversal.Common
+{
+    public interface IConnectionFactory
+    {
+        // SE CREA UN METODO PARA INTERACTUAR CON EL GESTOR DE BASE DE DATOS
+        IDbConnection GetConnection { get; }
+    }
+}
