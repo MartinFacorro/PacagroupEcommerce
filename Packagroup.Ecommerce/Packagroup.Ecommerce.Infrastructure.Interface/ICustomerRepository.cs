15f3ab7761c937a653430ce60c1e8d0dc49b0056
Martin Facorro <mfacorro@live.com.ar>
2023-05-01 12:52:22 -0300

    14. Construcción de la Capa de Infraestructura de Persistencia de Datos Min 28:15

--- Packagroup.Ecommerce/Packagroup.Ecommerce.Infrastructure.Interface/ICustomerRepository.cs
+++ Packagroup.Ecommerce/Packagroup.Ecommerce.Infrastructure.Interface/ICustomerRepository.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using Packagroup.Ecommerce.Domain.Entity;
+using System.Threading.Tasks;
 
 
 namespace Packagroup.Ecommerce.Infrastructure.Interface
 {
-    internal interface ICustomerRepository
+    public interface ICustomerRepository
     {
+        //METODOS DEl OBJETO DE DOMINIO CUSTOMERS
+
+        #region METODO SINCRONOS
+        bool Insert(Customer customer);
+        bool Update(Customer customer);
+        bool Delete(string CustomerId);
+        Customer Get(string CustomerId);
+        IEnumerable<Customer> GetAll();
+        #endregion
+
+
+        #region METODO ASINCRONOS
+        Task<bool> InsertAsync(Customer customer);
+        Task<bool> UpdateAsync(Customer customer);
+        Task<bool> DeleteAsync(string CustomerId);
+        Task<Customer> GetAsync(string CustomerId);
+        Task<IEnumerable<Customer>> GetAllAsync();
+        #endregion
+
+
     }
 }
---
+++ Packagroup.Ecommerce/Packagroup.Ecommerce.Infrastructure.Repository/CustomerRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Packagroup.Ecommerce.Domain.Entity;
+using Packagroup.Ecommerce.Infrastructure.Interface;
+using Packagroup.Ecommerce.Transversal.Common;
+using Dapper;
+using System.Data;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Packagroup.Ecommerce.Infrastructure.Repository
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+
+        private readonly IConnectionFactory _connectionFactory;
+
+        public CustomerRepository(IConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+        #region Metodos Sincronos
+        public bool Insert(Customer customer)
+        {
+            using (var connection = _connectionFactory)
+            {
+                var query = "CustomersInsert";
+
+                var parameters = new DynamicParameters();
+
+                parameters.add("Customerld", customer.Customerld);
+                parameters.add("CompanyName ", customer.CompanyName);
+                parameters.add("ContactName", customer.ContactName);
+                parameters.add("ContactTit1e", customer.ContactTit1e);
+                parameters.add("Address", customer.Address);
+                parameters.add("City", customer.City);
+                parameters.add("Region", customer.Region);
+                parameters.add("PostalCode", customer.PostalCode);
+                parameters.add("Country", customer.Country);
+                parameters.add("Phone", customer.Phone);
+                parameters.add("Fax ", customer.Fax);
+            }
+        }
+
+        bool Update(Customer customer);
+        bool Delete(string CustomerId);
+        Customer Get(string CustomerId);
+        IEnumerable<Customer> GetAll();
+        #endregion
+
+
+    }
+}
