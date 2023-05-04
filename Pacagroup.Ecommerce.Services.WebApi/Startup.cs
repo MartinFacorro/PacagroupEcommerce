437627847b9c9eba43592b0d32f104de0f36eb9f
Martin Facorro <mfacorro@live.com.ar>
2023-05-04 10:15:19 -0300

    19. Habilitar Solicitudes de Origen Cruzado (CORS)

--- Pacagroup.Ecommerce.Services.WebApi/Startup.cs
+++ Pacagroup.Ecommerce.Services.WebApi/Startup.cs
@@ -19,11 +19,15 @@ using Pacagroup.Ecommerce.Domain.Interface;
 using Pacagroup.Ecommerce.Domain.Core;
 using Pacagroup.Ecommerce.Application.Interface;
 using Pacagroup.Ecommerce.Application.Main;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Reflection;
+using System.IO;
 
 namespace Pacagroup.Ecommerce.Services.WebApi
 {
     public class Startup
     {
+        readonly string myPolicy = "policyApiEcommerce";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -35,6 +39,9 @@ namespace Pacagroup.Ecommerce.Services.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddAutoMapper(x => x.AddProfile(new MappingsProfile()));
+            services.AddCors(options => options.AddPolicy(myPolicy, builder => builder.WithOrigins(Configuration["Config:OriginCors"])
+                                                                                        .AllowAnyHeader()
+                                                                                        .AllowAnyMethod()));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
                 .AddJsonOptions(options => { options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver(); });
 
@@ -43,6 +50,33 @@ namespace Pacagroup.Ecommerce.Services.WebApi
             services.AddScoped<ICustomersApplication, CustomersApplication>();
             services.AddScoped<ICustomersDomain, CustomersDomain>();
             services.AddScoped<ICustomersRepository, CustomersRepository>();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Pacagroup Technology Services API Market",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Alex Espejo",
+                        Email = "alex.espejo.c@gmail.com",
+                        Url = "https://pacagroup.com"
+                    },
+                    License = new License
+                    {
+                        Name = "Use under LICX",
+                        Url = "https://example.com/license"
+                    }
+                });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +86,18 @@ namespace Pacagroup.Ecommerce.Services.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API Ecommerce V1");
+            });
+
+            app.UseCors(myPolicy);
 
             app.UseMvc();
         }
