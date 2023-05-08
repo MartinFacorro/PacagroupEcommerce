2409b3852e262c9fe655e43d4f025ae1d031884c
Martin Facorro <mfacorro@live.com.ar>
2023-05-08 14:59:12 -0300

    21. Habilitar Seguridad en Web API utilizando JSON Web Token (JWT) - Parte I
    MIN 13:31

---
+++ Pacagroup.Ecommerce.Domain.Core/UsersDomain.cs
@@ -0,0 +1,22 @@
+﻿using Pacagroup.Ecommerce.Domain.Entity;
+using Pacagroup.Ecommerce.Domain.Interface;
+using Pacagroup.Ecommerce.Infrastructure.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pacagroup.Ecommerce.Domain.Core
+{
+    public class UsersDomain : IUsersDomain
+    {
+        private readonly IUsersRepository _userRepository;
+        public UsersDomain(IUsersRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public Users Authenticate(string username, string password)
+        { 
+            return _userRepository.Authenticate(username, password);
+        }
+    }
+}
---
+++ Pacagroup.Ecommerce.Infrastructure.Repository/UsersRepository.cs
@@ -0,0 +1,34 @@
+﻿using Pacagroup.Ecommerce.Infrastructure.Interface;
+using Pacagroup.Ecommerce.Transversal.Common;
+using Pacagroup.Ecommerce.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Dapper;
+using System.Data;
+
+namespace Pacagroup.Ecommerce.Infrastructure.Repository
+{
+    public class UsersRepository : IUsersRepository
+    {
+        private readonly IConnectionFactory _connectionFactory;
+
+        public UsersRepository(IConnectionFactory connectionFactory)   
+        { 
+            _connectionFactory = connectionFactory;
+        }
+
+        public Users Authenticate(string userName, string password)
+        {
+            using (var connection = _connectionFactory.GetConnection)
+            {
+                var query = "UserGetByUserAndPassword";
+                var parameters = new DynamicParameters();
+                parameters.Add("UserName", userName);
+                parameters.Add("Password", password);
+
+                var user = connection.QuerySingle<Users>(query, param: parameters, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
